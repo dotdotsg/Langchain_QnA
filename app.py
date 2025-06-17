c56ecfdd841d7c409f441867581f9d178179d920
Sumeet Gupta <sumeet13gupta@gmail.com>
2025-06-17 23:29:09 +0530

    removed the deprecated model used the langchain community

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+from langchain_community.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.llms import openai
 import streamlit as st
 from dotenv import load_dotenv
 import os
 
 # Load environment variables from .env file
 load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 def get_openai_response(question, model):
-    api_key = os.getenv("OPENAI_API_KEY")
     if model == "gpt-3.5-turbo":
-        llm = ChatOpenAI(model_name=model, temperature=0.7, openai_api_key=api_key)
+        llm = ChatOpenAI(model_name=model, temperature=0.7, openai_api_key=openai_api_key)
         return llm.predict(question)
-    else:  # text-davinci-003 or other completion models
-        llm = OpenAI(model_name=model, temperature=0.7, openai_api_key=api_key)
-        return llm(question)
-    # llm = OpenAI(get_openai_api_key=os.getenv("OPENAI_API_KEY"), model_name="gpt-3.5-turbo", temperature=0.7)
-    # response = llm(question)
-    # return response;
-
-    # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7, openai_api_key=os.getenv("OPENAI_API_KEY"))
-    # return llm.predict(question)
 
 # Initialize the streamlit app
 
@@ -28,7 +20,7 @@ st.set_page_config(page_title="QnA with OpenAI", page_icon=":robot_face:")
 
 st.title("QnA with OpenAI")
 st.header("A Langchain Application for QnA with OpenAI")
-model = st.selectbox("Select OpenAI model:", ["gpt-3.5-turbo", "text-davinci-003"])
+model = st.selectbox("Select OpenAI model:", ["gpt-3.5-turbo"])
 question = st.text_input("Enter your question:", key="input")
 response = get_openai_response(question, model) if question else None
 submit = st.button("Submit")
